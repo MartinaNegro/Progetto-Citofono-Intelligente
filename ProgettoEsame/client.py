97394733299b66a23476b55501cd996bef076773
MartinaNegro <220082@studenti.unimore.it>
2023-02-17 00:45:58 +0100

    commit

---
+++ ProgettoEsame/client.py
@@ -0,0 +1,75 @@
+########### LIBRERIE ###########
+
+import cv2
+from simple_facerec import SimpleFacerec
+import datetime
+from secret import secret
+import os
+from requests import post
+
+
+########### URL DEL SERVER DI GCP ###########
+
+base_url = 'https://progettoesame-378011.appspot.com/'
+
+
+########### UTILIZZO MODULO SIMPLEFACEREC IMPORTATO DA SIMPLE_FACEREC.PY ###########
+
+sfr = SimpleFacerec()
+sfr.load_encoding_images("images/")
+
+
+########### FUNZIONAMENTO DEL VIDEOCITOFONO ###########
+
+old_name = []
+current_name = []
+name_images = []
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    face_locations, face_names = sfr.detect_known_faces(frame)
+    date = datetime.datetime.now()
+    h = date.time().strftime("%H:%M:%S")
+    cv2.putText(frame, h, (10, 50), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+
+    ########### RILEVAMENTO VOLTI E IDENTIFICAZIONE ###########
+
+    for face_loc, name in zip(face_locations, face_names):
+        y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 4)
+        cv2.rectangle(frame, (x2, y1 -30), (x1, y1), (0, 255, 0), cv2.FILLED)
+        cv2.putText(frame, name, (x1,y1-10), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255), 1)
+
+
+        ########### INVIO INFORMAZIONI PREMENDO IL CAMPANELLO ###########
+
+        if cv2.waitKey(1) & 0xFF == ord('s'):
+            name_image = f'{h}_{name}.png'
+            cv2.imwrite(name_image, frame)
+            files = {'file': open(name_image, 'rb')}
+            r = post(f'{base_url}/sensors/cam', data={'name': name, 'time': h, 'secret': secret}, files=files)
+            name_images.append(name_image)
+
+        current_name = face_names
+
+    cv2.imshow("Videocitofono", frame)
+
+
+    if old_name != current_name:
+        old_name = current_name
+        print('This is ', name,' at ', h)
+
+
+    ########### SPEGNIMENTO VIDEOCITOFONO ###########
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        for name in name_images:
+            os.remove(name)
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ ProgettoEsame/main.py
@@ -0,0 +1,85 @@
+########### LIBRERIE ###########
+
+from flask import Flask, request, render_template, send_file
+from google.cloud import firestore, storage
+from werkzeug.utils import secure_filename
+from secret import secret
+import os
+
+
+########### CREAZIONE APPLICAZIONE FLASK ###########
+
+app = Flask(__name__)
+
+
+########### PAGINA INIZIALE ###########
+
+@app.route('/home', methods=['GET'])
+@app.route('/', methods=['GET'])
+def main():
+    return render_template("home.html")
+
+
+########### SALVATAGGIO DATI SU DATABASE FIRESTORE E CLOUD STORAGE ###########
+
+@app.route('/sensors/cam', methods=['POST'])
+def save_data():
+    s = request.values['secret']
+    if s == secret:
+
+        name = request.values['name']
+        time = request.values['time']
+        db = firestore.Client()
+        db.collection('cam').document(time).set({'individuo': name, 'ora': time})
+
+
+        file = request.files['file']
+        fname = secure_filename(file.filename)
+        file.save(os.path.join('/tmp/', fname))
+
+        client = storage.Client()
+        bucket = client.bucket('raccolta-foto')
+        source_file_name = fname
+        destination_blob_name = source_file_name
+        blob = bucket.blob(destination_blob_name)
+        blob.upload_from_filename(os.path.join('/tmp/', fname))
+
+
+########### OUTPUT: CONVERSIONE DATI DEL DATABASE IN DIZIONARIO ###########
+
+@app.route('/sensors/cam', methods=['GET'])
+def read_all():
+    db = firestore.Client()
+    data = []
+    for doc in db.collection('cam').stream():
+        x = doc.to_dict()
+        data.append([x['individuo'],  x['ora']])
+    return data
+
+
+########### OUTPUT: ACCESSO ALLA TABELLA DEI DATI CONVERTITI ###########
+
+@app.route('/table', methods=['GET'])
+def create_table():
+    data = read_all()
+    return render_template('table.html', data=data)
+
+
+########### OUTPUT: ACCESSO ALLE IMMAGINI DEL CLOUD STORAGE ###########
+
+@app.route('/photo', methods=['GET'])
+def view_screen():
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket('raccolta-foto')
+    filename = [filename.name for filename in list(bucket.list_blobs(prefix=''))]
+
+    im = filename[len(filename)-1]
+    blob = bucket.blob(im)
+    blob.download_to_filename(os.path.join('/tmp/', im))
+    return send_file(os.path.join('/tmp/', im), mimetype='image/jpeg')
+
+
+########### ESECUZIONE FLASK SULLA PORTA 8080 ###########
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
